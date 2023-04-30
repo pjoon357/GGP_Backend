70fe5ac42a1f0c0c448555b5cdf74bde3f6fab3d
pjoon357 <pjoon357@naver.com>
2023-04-30 20:11:03 +0900

    init

---
+++ src/test/java/com/example/GGP/GgpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GGP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
